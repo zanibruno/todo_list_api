61e416135821bd67c0b149ef061e4ca725cf8475
zanibruno <corporate.zanin@gmail.com>
2020-03-26 12:17:31 +0000

    created new rails with API flag

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,48 @@
+class ItemsController < ApplicationController
+    before_action :get_item, only: [:show, :update, :destroy]
+
+    def index 
+        items = Item.all
+    
+        render json: items
+    end 
+
+    def show
+        get_item 
+    
+        render json: item 
+    end 
+
+    def create 
+        list = List.find_by(id: params[:list_id])
+        item = list.items.build(item_params)
+        if item.save 
+            render json: item 
+        else 
+            render json: {message: "Item not created"}
+        end 
+    end 
+
+    def update 
+        get_item 
+        item.update(item_params)
+
+        render json: item 
+    end 
+
+    def destroy
+        item.destroy
+        render json: {itemId: item.id}
+    end 
+
+
+    private 
+
+    def get_item 
+        item = Item.find_by(id: params[:id])
+    end 
+
+    def item_params
+        params.require(:item).permit(:name, :description, :list_id)
+    end 
+end
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,54 @@
+# class ListsController < ApplicationController
+#     before_action :get_lists, only: [:show, :update, :destroy]
+#
+#     def index
+#         lists = List.all
+#         render json: lists.to_json(:include => {:items => {:only => [:id, :name, :description, :list_id]}})
+#     end
+#
+#     def show
+#         get_list
+#         if list
+#             render json: list.to_json(:include => {:items => {:only => [:id, :name, :description, :list_id]}})
+#         else
+#             render json: {message: "List not found"}
+#         end
+#     end
+#
+#     def create
+#         list = List.new(list_params)
+#         if list.save
+#             render json: list
+#         else
+#             render json: {message: "list not created"}
+#         end
+#     end
+#
+#     def update
+#         get_list
+#         if list.update(list_params)
+#             render json: list
+#         else
+#             render json: {message: "list not updated"}
+#         end
+#     end
+#
+#     def destroy
+#         get_list
+#         item.delete
+#
+#         render json: {listId: list.id}
+#     end
+#
+#
+#
+#     private
+#
+#     def get_list
+#         list = List.find_by(id: params[:id ])
+#     end
+#
+#     def list_params
+#         params.require(:list).permit(:name)
+#     end
+# end
